46deb8359ae3e119fad91cc49d209e4d29983c1b
sdatta97 <sdatta@nyu.edu>
2023-04-20 11:35:36 +0530

    Lists corrected

--- Figure6_execute.sh
+++ Figure6_execute.sh
@@ -4,9 +4,9 @@
 # sysctl -w net.core.wmem_default=2147483647
 # sysctl -w net.core.rmem_max=2147483647
 # sysctl -w net.core.wmem_max=2147483647
-rtt_list = 5 10 25 50 75 100 150 200
-bandwidth_list = 10 20 50 100 250 500 750 1000
-bufcap_list = 100 10000
+rtt_list = {5 10 25 50 75 100 150 200}
+bandwidth_list = {10 20 50 100 250 500 750 1000}
+bufcap_list = {100 10000}
 
 for bufcap in bufcap_list 
 do
