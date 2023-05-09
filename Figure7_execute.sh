38eab8df19e71197f8247f2e7c67a8493a55dce7
sdatta97 <sdatta@nyu.edu>
2023-05-09 16:07:11 +0530

    Code for Fig7

---
+++ Figure7_execute.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+##code runs on router (tbf)
+# sysctl -w net.core.rmem_default=2147483647
+# sysctl -w net.core.wmem_default=2147483647
+# sysctl -w net.core.rmem_max=2147483647
+# sysctl -w net.core.wmem_max=2147483647
+
+sudo ssh -o StrictHostKeyChecking=no -T root@h3 "mkdir -p fig6"
+sudo ssh -o StrictHostKeyChecking=no -T root@h1 "mkdir -p fig6"
+bandwidth = 100;
+bufcap = 10000;
+rtt = 25;
+loss = 1 2 3 6 12 18 27 36 45;
+for loss_pc in loss
+    ## Delete any existing queues
+    sudo tc qdisc del dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") root  
+    ## Create an htb qdisc
+    sudo tc qdisc replace dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") root handle 1: htb default 3  
+    ## Limit the queue traffic to the bandwidth
+    sudo tc class add dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") parent 1: classid 1:3 htb rate "$bandwidth"Mbit
+    ## Set up queue limit
+    sudo tc qdisc add dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") parent 1:3 bfifo limit "$bufcap"kbit
+    ## Delete any existing queues
+    sudo tc qdisc del dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") root
+    ## Create an htb qdisc
+    sudo tc qdisc replace dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") root handle 1: htb default 3
+    ## Limit the queue traffic to the bandwidth
+    sudo tc class add dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") parent 1: classid 1:3 htb rate "$bandwidth"Mbit
+    ## Set up queue limit
+    sudo tc qdisc add dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") parent 1:3 bfifo limit "$bufcap"kbit
+    ## Set up network delay 
+    sudo tc qdisc replace dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") root netem delay "$rtt"ms
+    sudo tc qdisc replace dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") root netem loss "$loss_pc"%
+
+    sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
+    sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C cubic -n 100mb -fk | tee ./fig6/"$bufcap"_"$bandwidth"_"$rtt"_"$loss_pc"_cubic.txt"
+    sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
+    sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C bbr -n 100mb -fk | tee ./fig6/"$bufcap"_"$bandwidth"_"$rtt"_"$loss_pc"_bbr.txt"
+done
