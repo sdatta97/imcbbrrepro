3f7700ec4a7aa39599b4ba44058186daf23fa75a
sdatta97 <sdatta@nyu.edu>
2023-05-12 09:51:46 +0530

    Plotting of fig6 adapted to new output

--- fig6_visualize.py
+++ fig6_visualize.py
@@ -4,95 +4,44 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt 
 
 # reading the CSV file
-val_arr = np.array(pd.read_csv('bbr_10mb.csv'))
-latency_bbr_10mb = np.ones((2,8,8))
+val_arr = np.array(pd.read_csv('bbr_100mb.csv'))
+latency_bbr_100kb = np.ones((8,8))
+latency_bbr_10mb = np.ones((8,8))
 arr_length = val_arr.size
-counter = int(0)
-i = 0
-while i<arr_length:
-    bufcap_idx = int(np.floor_divide((i+counter)/2, 64))
-    bandwidth_idx = int(np.floor_divide(np.remainder((i+counter)/2, 64),8))
-    rtt_idx = int(np.remainder(np.remainder((i+counter)/2, 64),8))
+for i in range(arr_length):
+    bufcap_idx = int(np.floor_divide(i, 64))
+    bandwidth_idx = int(np.floor_divide(np.remainder(i, 64),8))
+    rtt_idx = int(np.remainder(np.remainder(i, 64),8))
     print(bufcap_idx , bandwidth_idx , rtt_idx)
-    if val_arr[i] == -1:
-        counter = counter + 1
-        latency_bbr_10mb [bufcap_idx, bandwidth_idx, rtt_idx] = 0
-        i = i+1
+    if bufcap_idx == 0:
+        latency_bbr_100kb [bandwidth_idx, rtt_idx] = val_arr[i]
     else:
-        latency_bbr_10mb [bufcap_idx, bandwidth_idx, rtt_idx] = -int(val_arr[i].split('-'))
-        i = i+2
-print(counter)
+        latency_bbr_10mb [bandwidth_idx, rtt_idx] = val_arr[i]
+print(latency_bbr_100kb)
 print(latency_bbr_10mb)
 
-val_arr = np.array(pd.read_csv('bbr_100mb.csv'))
-latency_bbr_100mb = np.ones((2,8,8))
-arr_length = val_arr.size
-counter = int(0)
-i = 0
-while i<arr_length:
-    bufcap_idx = int(np.floor_divide((i+counter)/2, 64))
-    bandwidth_idx = int(np.floor_divide(np.remainder((i+counter)/2, 64),8))
-    rtt_idx = int(np.remainder(np.remainder((i+counter)/2, 64),8))
-    print(bufcap_idx , bandwidth_idx , rtt_idx)
-    if val_arr[i] == -1:
-        counter = counter + 1
-        latency_bbr_10mb [bufcap_idx, bandwidth_idx, rtt_idx] = 0
-        i = i+1
-    else:
-        print(-val_arr[i])
-        latency_bbr_100mb [bufcap_idx, bandwidth_idx, rtt_idx] = -val_arr[i]
-        i = i+2
-print(counter)
-print(latency_bbr_100mb)
-
-val_arr = np.array(pd.read_csv('cubic_10mb.csv'))
-latency_cubic_10mb = np.ones((2,8,8))
-arr_length = val_arr.size
-counter = int(0)
-i = 0
-while i<arr_length:
-    bufcap_idx = int(np.floor_divide((i+counter)/2, 64))
-    bandwidth_idx = int(np.floor_divide(np.remainder((i+counter)/2, 64),8))
-    rtt_idx = int(np.remainder(np.remainder((i+counter)/2, 64),8))
-    print(bufcap_idx , bandwidth_idx , rtt_idx)
-    if val_arr[i] == -1:
-        counter = counter + 1
-        latency_cubic_10mb [bufcap_idx, bandwidth_idx, rtt_idx] = 0
-        i = i+1
-    else:
-        print(-val_arr[i])
-        latency_cubic_10mb [bufcap_idx, bandwidth_idx, rtt_idx] = -val_arr[i]
-        i = i+2
-print(counter)
-print(latency_cubic_10mb)
-
 val_arr = np.array(pd.read_csv('cubic_100mb.csv'))
-latency_cubic_100mb = np.ones((2,8,8))
+latency_cubic_100kb = np.ones((8,8))
+latency_cubic_10mb = np.ones((8,8))
 arr_length = val_arr.size
-counter = int(0)
-i = 0
-while i<arr_length:
-    bufcap_idx = int(np.floor_divide((i+counter)/2, 64))
-    bandwidth_idx = int(np.floor_divide(np.remainder((i+counter)/2, 64),8))
-    rtt_idx = int(np.remainder(np.remainder((i+counter)/2, 64),8))
+for i in range(arr_length):
+    bufcap_idx = int(np.floor_divide(i, 64))
+    bandwidth_idx = int(np.floor_divide(np.remainder(i, 64),8))
+    rtt_idx = int(np.remainder(np.remainder(i, 64),8))
     print(bufcap_idx , bandwidth_idx , rtt_idx)
-    if val_arr[i] == -1:
-        counter = counter + 1
-        latency_cubic_10mb [bufcap_idx, bandwidth_idx, rtt_idx] = 0
-        i = i+1
+    if bufcap_idx == 0:
+        latency_cubic_100kb [bandwidth_idx, rtt_idx] = val_arr[i]
     else:
-        print(-val_arr[i])
-        latency_cubic_100mb [bufcap_idx, bandwidth_idx, rtt_idx] = -val_arr[i]
-        i = i+2
-print(counter)
-print(latency_cubic_100mb)
+        latency_cubic_10mb [bandwidth_idx, rtt_idx] = val_arr[i]
+print(latency_cubic_100kb)
+print(latency_cubic_10mb)
 
 latdec_100kb = np.ones((8,8))
 latdec_10mb = np.ones((8,8))
 for bandwidth_idx in range(8):
     for rtt_idx in range(8):
-        latdec_100kb[bandwidth_idx,rtt_idx] = np.divide((latency_bbr_100mb[0,bandwidth_idx,rtt_idx] - latency_cubic_100mb[0,bandwidth_idx,rtt_idx]), latency_cubic_100mb[0,bandwidth_idx,rtt_idx])
-        latdec_10mb[bandwidth_idx,rtt_idx]  = np.divide((latency_bbr_100mb[1,bandwidth_idx,rtt_idx] - latency_cubic_100mb[1,bandwidth_idx,rtt_idx]), latency_cubic_100mb[1,bandwidth_idx,rtt_idx])
+        latdec_100kb[bandwidth_idx,rtt_idx] = np.divide((latency_bbr_100kb[bandwidth_idx,rtt_idx] - latency_cubic_100kb[bandwidth_idx,rtt_idx]), latency_cubic_100kb[bandwidth_idx,rtt_idx])
+        latdec_10mb[bandwidth_idx,rtt_idx]  = np.divide((latency_bbr_10mb[bandwidth_idx,rtt_idx] - latency_cubic_10mb[bandwidth_idx,rtt_idx]), latency_cubic_10mb[bandwidth_idx,rtt_idx])
 print(latdec_100kb)
 fig, ax = plt.subplots()
 im = ax.imshow(latdec_100kb)
