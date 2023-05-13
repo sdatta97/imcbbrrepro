d90c199ba23223565f78a6a0f2acf2dbf51f6e3a
sdatta97 <sdatta@nyu.edu>
2023-05-13 15:03:10 +0530

    Change bandwidth unit

--- Figure5_execute.sh
+++ Figure5_execute.sh
@@ -29,7 +29,7 @@ do
                 ## Create an htb qdisc
                 sudo tc qdisc replace dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") root handle 1: htb default 3
                 ## Limit the queue traffic to the bandwidth
-                sudo tc class add dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") parent 1: classid 1:3 htb rate "$bandwidth"Mbit
+                sudo tc class add dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") parent 1: classid 1:3 htb rate "$bandwidth"mbps
                 ## Set up queue limit
                 sudo tc qdisc add dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") parent 1:3 bfifo limit "$bufcap"kb
                 sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
