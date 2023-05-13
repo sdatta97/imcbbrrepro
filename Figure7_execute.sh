341fba2e93ffe1824a8294743a1c66bbd3ce9b38
sdatta97 <sdatta@nyu.edu>
2023-05-13 12:15:51 +0530

    Added trial ID, fixed netem issues

--- Figure7_execute.sh
+++ Figure7_execute.sh
@@ -12,32 +12,36 @@ sudo ssh -o StrictHostKeyChecking=no -T root@h1 "mkdir -p fig7"
 ## rtt = 25;
 for loss_pc in 0 1 2 3 6 12 18 27 36 45 50
 do
-    ## Delete any existing queues
-    sudo tc qdisc del dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") root  
-    ## Create an htb qdisc
-    sudo tc qdisc replace dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") root handle 1: htb default 3  
-    ## Limit the queue traffic to the bandwidth
-    sudo tc class add dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") parent 1: classid 1:3 htb rate 100Mbit
-    ## Set up queue limit
-    sudo tc qdisc add dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") parent 1:3 bfifo limit 10Mb
-    ## Delete any existing queues
-    sudo tc qdisc del dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") root
-    ## Create an htb qdisc
-    sudo tc qdisc replace dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") root handle 1: htb default 3
-    ## Limit the queue traffic to the bandwidth
-    sudo tc class add dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") parent 1: classid 1:3 htb rate 100Mbit
-    ## Set up queue limit
-    sudo tc qdisc add dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") parent 1:3 bfifo limit 10Mb
-    ## Set up network delay 
-    sudo tc qdisc replace dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") root netem delay 25ms
-    sudo tc qdisc replace dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") root netem loss $loss_pc%
-
-    sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
-    sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C cubic -t 60s -fk | tee ./fig7/"$loss_pc"_cubic.txt"
-    sleep 10
-    sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
-    sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C bbr -t 60s -fk | tee ./fig7/"$loss_pc"_bbr.txt"
-    sleep 10
-    sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
-    sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C reno -t 60s -fk | tee ./fig7/"$loss_pc"_reno.txt"
+    for trial in 1 2 3 4 5
+    do
+        ## Delete any existing queues
+        sudo tc qdisc del dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") root  
+        ## Set up network delay 
+        sudo tc qdisc replace dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") root netem delay 25ms
+        ## Add loss to network
+        sudo tc qdisc replace dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") root netem loss $loss_pc%
+        ## Create an htb qdisc
+        ## sudo tc qdisc replace dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") root handle 1: htb default 3  
+        ## Limit the queue traffic to the bandwidth
+        ## sudo tc class add dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") parent 1: classid 1:3 htb rate 100Mbit
+        ## Set up queue limit
+        ## sudo tc qdisc add dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") parent 1:3 bfifo limit 10Mb
+        ## Delete any existing queues
+        sudo tc qdisc del dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") root
+        ## Create an htb qdisc
+        sudo tc qdisc replace dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") root handle 1: htb default 3
+        ## Limit the queue traffic to the bandwidth
+        sudo tc class add dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") parent 1: classid 1:3 htb rate 100Mbit
+        ## Set up queue limit
+        sudo tc qdisc add dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") parent 1:3 bfifo limit 10Mb
+        sleep 10
+        sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
+        sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C cubic -t 60s -fk | tee ./fig7/"$loss_pc"_"$trial"_cubic.txt"
+        sleep 10
+        sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
+        sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C bbr -t 60s -fk | tee ./fig7/"$loss_pc"_"$trial"_bbr.txt"
+        sleep 10
+        sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
+        sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C reno -t 60s -fk | tee ./fig7/"$loss_pc"_"$trial"_reno.txt"
+    done
 done
