2e61fed82ff8adc8abb8f1b8d8734f0d1d2dba13
sdatta97 <sdatta@nyu.edu>
2023-05-14 11:12:31 +0530

    add trial id

--- fig7_visualize.sh
+++ fig7_visualize.sh
@@ -6,10 +6,13 @@ touch bbr_retr.csv
 touch cubic_retr.csv
 for loss_pc in 0 1 2 3 6 12 18 27 36 45 50
 do
-    cat "$loss_pc"_bbr.txt | grep "Bitrate" -A 1 | grep -v "Bitrate" | tail -n 1 | awk '{print $7}' >> bbr_goodput.csv
-    cat "$loss_pc"_cubic.txt | grep "Bitrate" -A 1 | grep -v "Bitrate" | tail -n 1 | awk '{print $7}' >> cubic_goodput.csv
-    cat "$loss_pc"_reno.txt | grep "Bitrate" -A 1 | grep -v "Bitrate" | tail -n 1 | awk '{print $7}' >> reno_goodput.csv
-    cat "$loss_pc"_bbr.txt | grep "Bitrate" -A 1 | grep -v "Bitrate" | tail -n 1 | awk '{print $9}' >> bbr_retr.csv
-    cat "$loss_pc"_cubic.txt | grep "Bitrate" -A 1 | grep -v "Bitrate" | tail -n 1 | awk '{print $9}' >> cubic_retr.csv 
-    cat "$loss_pc"_reno.txt | grep "Bitrate" -A 1 | grep -v "Bitrate" | tail -n 1 | awk '{print $9}' >> reno_retr.csv
+    for trial in 1
+    do 
+        cat "$loss_pc"_"$trial"_bbr.txt | grep "Bitrate" -A 1 | grep -v "Bitrate" | tail -n 1 | awk '{print $7}' >> bbr_goodput.csv
+        cat "$loss_pc"_"$trial"_cubic.txt | grep "Bitrate" -A 1 | grep -v "Bitrate" | tail -n 1 | awk '{print $7}' >> cubic_goodput.csv
+        cat "$loss_pc"_"$trial"_reno.txt | grep "Bitrate" -A 1 | grep -v "Bitrate" | tail -n 1 | awk '{print $7}' >> reno_goodput.csv
+        cat "$loss_pc"_"$trial"_bbr.txt | grep "Bitrate" -A 1 | grep -v "Bitrate" | tail -n 1 | awk '{print $9}' >> bbr_retr.csv
+        cat "$loss_pc"_"$trial"_cubic.txt | grep "Bitrate" -A 1 | grep -v "Bitrate" | tail -n 1 | awk '{print $9}' >> cubic_retr.csv 
+        cat "$loss_pc"_"$trial"_reno.txt | grep "Bitrate" -A 1 | grep -v "Bitrate" | tail -n 1 | awk '{print $9}' >> reno_retr.csv
+    done
 done
