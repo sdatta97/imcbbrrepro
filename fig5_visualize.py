141889ad4076753a079ed6a5f5ce12bee5373917
sdatta97 <sdatta@nyu.edu>
2023-05-15 12:49:04 +0530

    Fixed plotting axes, added labels and colorbar

--- fig5_visualize.py
+++ fig5_visualize.py
@@ -56,38 +56,63 @@ for bandwidth_idx in range(8):
     for rtt_idx in range(8):
         gpgain_100kb[bandwidth_idx,rtt_idx] = np.divide((goodput_bbr[0,bandwidth_idx,rtt_idx] - goodput_cubic[0,bandwidth_idx,rtt_idx]), goodput_cubic[0,bandwidth_idx,rtt_idx])
         gpgain_10mb[bandwidth_idx,rtt_idx]  = np.divide((goodput_bbr[1,bandwidth_idx,rtt_idx] - goodput_cubic[1,bandwidth_idx,rtt_idx]), goodput_cubic[1,bandwidth_idx,rtt_idx])
+
+RTT = ["5","10","25","50","75","100","150","200"]
+Bandwidth = ["10","20","50","100","250","500","750","1000"]
+
 print(gpgain_100kb)
 fig, ax = plt.subplots()
 im = ax.imshow(gpgain_100kb)
+# Show all ticks and label them with the respective list entries
+ax.set_xticks(np.arange(8), labels=RTT)
+ax.set_yticks(np.arange(8), labels=Bandwidth)
 for i in range(8):
     for j in range(8):
-        text = ax.text(j, i, format(gpgain_100kb[i, j],'.2f'), ha="center", va="center", color="w")
+        text = ax.text(i, j, format(gpgain_100kb[j, i],'.2f'), ha="center", va="center", color="w")
 fig.tight_layout()
+plt.xlabel("RTT (in ms)")
+plt.ylabel("Bandwidth (in mbps)")
+plt.colorbar(im, label="Goodput gain", orientation="vertical")
 plt.show()
 
 print(gpgain_10mb)
 fig, ax = plt.subplots()
 im = ax.imshow(gpgain_10mb)
+ax.set_xticks(np.arange(8), labels=RTT)
+ax.set_yticks(np.arange(8), labels=Bandwidth)
 for i in range(8):
     for j in range(8):
-        text = ax.text(j, i, format(gpgain_10mb[i, j],'.2f'), ha="center", va="center", color="w")
+        text = ax.text(i, j, format(gpgain_10mb[j, i],'.2f'), ha="center", va="center", color="w")
 fig.tight_layout()
+plt.xlabel("RTT (in ms)")
+plt.ylabel("Bandwidth (in mbps)")
+plt.colorbar(im, label="Goodput gain", orientation="vertical")
 plt.show()
 
 print(retr_bbr)
 fig, ax = plt.subplots()
 im = ax.imshow(retr_bbr)
+ax.set_xticks(np.arange(8), labels=RTT)
+ax.set_yticks(np.arange(8), labels=Bandwidth)
 for i in range(8):
     for j in range(8):
         text = ax.text(j, i, format(retr_bbr[i, j],'.0f'), ha="center", va="center", color="w")
 fig.tight_layout()
+plt.xlabel("RTT (in ms)")
+plt.ylabel("Bandwidth (in mbps)")
+plt.colorbar(im, label="Retransmissions", orientation="vertical")
 plt.show()
 
 print(retr_cubic)
 fig, ax = plt.subplots()
 im = ax.imshow(retr_cubic)
+ax.set_xticks(np.arange(8), labels=RTT)
+ax.set_yticks(np.arange(8), labels=Bandwidth)
 for i in range(8):
     for j in range(8):
         text = ax.text(j, i, format(retr_cubic[i, j],'.0f'), ha="center", va="center", color="w")
 fig.tight_layout()
+plt.xlabel("RTT (in ms)")
+plt.ylabel("Bandwidth (in mbps)")
+plt.colorbar(im, label="Retransmissions", orientation="vertical")
 plt.show()
