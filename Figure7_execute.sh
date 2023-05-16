3cc920cdcc9e3ea55ab598687fd819a4d8666178
sdatta97 <sdatta@nyu.edu>
2023-05-16 12:38:42 +0530

    changing tee to >

--- Figure7_execute.sh
+++ Figure7_execute.sh
@@ -34,10 +34,10 @@ do
         sudo ssh -o StrictHostKeyChecking=no -T root@h3 "ping -c 1 h1"
         sudo ssh -o StrictHostKeyChecking=no -T root@h1 "ping -c 1 h3"
         sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
-        sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C cubic -t 60s -fk | tee ./fig7/"$loss_pc"_"$trial"_cubic.txt"
+        sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C cubic -t 60s -fk > ./fig7/"$loss_pc"_"$trial"_cubic.txt"
         sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
-        sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C bbr -t 60s -fk | tee ./fig7/"$loss_pc"_"$trial"_bbr.txt"
+        sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C bbr -t 60s -fk > ./fig7/"$loss_pc"_"$trial"_bbr.txt"
         sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
-        sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C reno -t 60s -fk | tee ./fig7/"$loss_pc"_"$trial"_reno.txt"
+        sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C reno -t 60s -fk > ./fig7/"$loss_pc"_"$trial"_reno.txt"
     done
 done
