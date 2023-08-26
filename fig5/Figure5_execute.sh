4f957834cf1bbb7786501e809abababec98b4d2f
sdatta97 <sdatta@nyu.edu>
2023-08-26 06:59:11 +0530

    adding socket buffer size

--- fig5/Figure5_execute.sh
+++ fig5/Figure5_execute.sh
@@ -31,10 +31,10 @@ do
                 ## sudo ssh -o StrictHostKeyChecking=no -T root@h1 "ping -c 1 h3"              
                 sleep 10
                 sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
-                sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C cubic -t 60s -fk -V > ./fig5/"$bufcap"_"$bandwidth"_"$rtt"_"$trial_idx"_cubic.txt"
+                sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C cubic -w 20M -t 60s -fk -V > ./fig5/"$bufcap"_"$bandwidth"_"$rtt"_"$trial_idx"_cubic.txt"
                 sleep 10
                 sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
-                sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C bbr2 -t 60s -fk -V > ./fig5/"$bufcap"_"$bandwidth"_"$rtt"_"$trial_idx"_bbr.txt"
+                sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C bbr2 -w 20M -t 60s -fk -V > ./fig5/"$bufcap"_"$bandwidth"_"$rtt"_"$trial_idx"_bbr.txt"
             done
         done
     done
