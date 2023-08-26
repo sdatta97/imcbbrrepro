2bb31f6ca64f614a6123113b19f69df84145f0be
sdatta97 <sdatta@nyu.edu>
2023-08-26 07:08:14 +0530

    Removing commented lines

--- fig5/Figure5_execute.sh
+++ fig5/Figure5_execute.sh
@@ -13,12 +13,6 @@ do
                 sudo tc qdisc del dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") root  
                 ## Set up network rtt 
                 sudo tc qdisc replace dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") root netem delay "$rtt"ms
-                ## Create an htb qdisc
-                ## sudo tc qdisc replace dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") root handle 1: htb default 3  
-                ## Limit the queue traffic to the bandwidth
-                ## sudo tc class add dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") parent 1: classid 1:3 htb rate "$bandwidth"Mbit
-                ## Set up queue limit
-                ## sudo tc qdisc add dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") parent 1:3 bfifo limit "$bufcap"kb
                 ## Delete any existing queues
                 sudo tc qdisc del dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") root
                 ## Create an htb qdisc
@@ -26,9 +20,7 @@ do
                 ## Limit the queue traffic to the bandwidth
                 sudo tc class add dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") parent 1: classid 1:3 htb rate "$bandwidth"Mbit
                 ## Set up queue limit
-                sudo tc qdisc add dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") parent 1:3 bfifo limit "$bufcap"kb
-                ## sudo ssh -o StrictHostKeyChecking=no -T root@h3 "ping -c 1 h1"
-                ## sudo ssh -o StrictHostKeyChecking=no -T root@h1 "ping -c 1 h3"              
+                sudo tc qdisc add dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") parent 1:3 bfifo limit "$bufcap"kb         
                 sleep 10
                 sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
                 sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C cubic -w 20M -t 60s -fk -V > ./fig5/"$bufcap"_"$bandwidth"_"$rtt"_"$trial_idx"_cubic.txt"
--- fig6/Figure6_execute.sh
+++ fig6/Figure6_execute.sh
@@ -13,12 +13,6 @@ do
                 sudo tc qdisc del dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") root  
                 ## Set up network rtt 
                 sudo tc qdisc replace dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") root netem delay "$rtt"ms
-                ## Create an htb qdisc
-                ## sudo tc qdisc replace dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") root handle 1: htb default 3  
-                ## Limit the queue traffic to the bandwidth
-                ## sudo tc class add dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") parent 1: classid 1:3 htb rate "$bandwidth"Mbit
-                ## Set up queue limit
-                ## sudo tc qdisc add dev $(ip route get 10.10.1.1 | grep -oP "(?<=dev )[^ ]+") parent 1:3 bfifo limit "$bufcap"kb
                 ## Delete any existing queues
                 sudo tc qdisc del dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") root
                 ## Create an htb qdisc
@@ -27,8 +21,6 @@ do
                 sudo tc class add dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") parent 1: classid 1:3 htb rate "$bandwidth"Mbit
                 ## Set up queue limit
                 sudo tc qdisc add dev $(ip route get 10.10.3.1 | grep -oP "(?<=dev )[^ ]+") parent 1:3 bfifo limit "$bufcap"kb
-                ## sudo ssh -o StrictHostKeyChecking=no -T root@h3 "ping -c 1 h1"
-                ## sudo ssh -o StrictHostKeyChecking=no -T root@h1 "ping -c 1 h3" 
                 sleep 10
                 sudo ssh -o StrictHostKeyChecking=no -T root@h3 "iperf3 -s -1 -D"
                 sudo ssh -o StrictHostKeyChecking=no -T root@h1 "iperf3 -c h3 -C cubic -n 10mb -fkV > ./fig6/"$bufcap"_"$bandwidth"_"$rtt"_"$trial"_cubic_10.txt"
